5862824b10bf85aae49bb433d478931b8a38c40f
Pericodes <52116925+pericodes@users.noreply.github.com>
2021-03-07 22:56:19 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+    function getWeb(String $url):string
+    {
+        $opciones = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>"Accept-language: es\r\n"
+            )
+        );
+        $html = file_get_contents($url, false, stream_context_create($opciones)); 
+        return $html ? $html : "";
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $param = json_decode(file_get_contents('php://input'), true);
+        if ($param && isset($param["url"])) {
+            try {
+                echo @json_encode(["html" => getWeb($param["url"])]);
+            } catch (\Throwable $th) {
+                echo json_encode(["Error" => "Error getting the html"]); 
+            }
+        }else{
+            echo json_encode(["Error" => "Url not sended"]); 
+        }
+    }else{
+        echo json_encode(["Error" => "Method not supported"]); 
+
+    }   
